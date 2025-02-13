0f457c5e4021f97cddef02917807a289fca00204
lihanrui2913 <11240696+lihanrui2913@user.noreply.gitee.com>
2025-02-13 19:07:35 +0800

    更新

--- rstd/src/proc.rs
+++ rstd/src/proc.rs
@@ -1,3 +1,15 @@
 pub fn exit(code: usize) -> isize {
     syscall!(sc::nr::EXIT, code)
 }
+
+pub fn fork() -> isize {
+    syscall!(sc::nr::FORK)
+}
+
+pub fn vfork() -> isize {
+    syscall!(sc::nr::VFORK)
+}
+
+pub fn wait4(pid: usize) -> isize {
+    syscall!(sc::nr::WAIT4, pid)
+}
